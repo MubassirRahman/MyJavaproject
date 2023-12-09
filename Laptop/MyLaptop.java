1cd12d609509a932caa3b14eaa3e391467fdb5e5
Zaid <9919mubashir@gmail.com>
2023-12-09 18:42:23 +0530

    New line added l1,g1,p1

--- Laptop/MyLaptop.java
+++ Laptop/MyLaptop.java
@@ -4,6 +4,10 @@ public class MyLaptop {
     public static void main(String[] args) {
         Laptop laptop=new Laptop();
         System.out.println(laptop);
+        Processor p1=new Processor();
+        Graphicscard g1=new Graphicscard();
+        Laptop l1= new Laptop(15, p1, "6GB", "1TB", g1,"XYZ", "simple");
+        System.out.println(l1);
     }
     
 }
